bc074fe079e91bdc36babd36d321d8e598f20bbd
fagyi <fagyipda@gmail.com>
2018-03-04 21:48:43 +0100

    pme: remove unused doze implementation

--- device.mk
+++ device.mk
@@ -200,10 +200,6 @@ PRODUCT_PACKAGES += \
 # PRODUCT_PACKAGES += \
 #    FlipFlap
 
-# Doze
-# PRODUCT_PACKAGES += \
-#     Doze
-
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
