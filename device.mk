8e8b587f6ba5c1c408e742562257f6a3829e3c70
fagyi <fagyipda@gmail.com>
2018-04-16 20:22:14 +0200

    pme: face detection and camera updates

--- device.mk
+++ device.mk
@@ -155,12 +155,14 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    com.qualcomm.qti.camera \
+    libmm-qcamera
 
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
