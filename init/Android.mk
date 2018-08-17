10ac9dedf180d5a8ac0610ef8511696f85631567
fagyi <fagyipda@gmail.com>
2018-08-16 21:30:53 -0500

    pme: update device specific init

--- init/Android.mk
+++ init/Android.mk
@@ -4,9 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libinit_pme
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := \
-	system/core/init \
-	system/core/base/include external/selinux/libselinux/include
+LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_pme.cpp
 LOCAL_STATIC_LIBRARIES := libbase
